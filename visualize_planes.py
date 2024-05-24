586d6bd8f8e8fa425e4154fc29ac3b7a0b88c8f2
Nathan Bailey <nathanbaileyw@gmail.com>
2024-05-24 09:46:27 +0100

    Update visualize_planes.py

--- visualize_planes.py
+++ visualize_planes.py
@@ -2,5 +2,5 @@
 
 import open3d as o3d
 
-mesh = o3d.io.read_point_cloud("wall_plane_2.ply", print_progress=True)
+mesh = o3d.io.read_point_cloud("walls/wall_plane_2.ply", print_progress=True)
 o3d.visualization.draw_geometries([mesh])
